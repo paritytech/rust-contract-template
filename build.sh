7bfe1e60db7c717b3d52e6b03ebb643d0b25b603
Alexander Theißen <alex.theissen@me.com>
2024-12-03 10:55:25 +0100

    Move to 64bit

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 cd "${0%/*}/"
 cargo build --release
-polkatool link --strip --output contract.polkavm target/riscv32emac-unknown-none-polkavm/release/contract
+polkatool link --strip --output contract.polkavm target/riscv64emac-unknown-none-polkavm/release/contract
